0fb256f5f4a47c07522fa1aa79e7379f29479257
Reinhardt <56647015+Reinhardt254@users.noreply.github.com>
2024-08-06 15:57:52 +0300

    completed shift unshift and pop link list methods

--- Linkedlist/linkedlist.js
+++ Linkedlist/linkedlist.js
@@ -24,8 +24,73 @@ class LinkedList {
       this.tail = newNode;
       this.length++;
    }
+
+   pop(){
+      if(!this.head){
+         return undefined;
+      }
+
+      let temp = this.head
+      let prev = this.head
+
+      while(temp.next){
+         prev = temp
+         temp = prev.next
+      }
+
+      this.tail = prev
+      this.tail.next = null
+      this.length --
+
+      if(this.length === 0){
+         this.head = null;
+         this.tail = null;
+      }
+
+      return temp;
+   }
+
+   unshift(value){
+      const newNode = new Node(value)
+
+      if(!this.head){
+         this.head = newNode;
+         this.tail = newNode;
+      }
+
+      newNode.next = this.head;
+      this.head = newNode;
+
+      this.length++;
+      return this;
+   }
+
+   shift(){
+      if(!this.head){
+         return undefined;
+      }
+      //1. Point the first node/element
+      let temp = this.head
+
+      //2. Move the head to the next node/element
+      this.head = this.head.next
+
+      //3. Remove first element
+      temp.next = null
+
+      this.length--
+
+      if(this.length === 0){
+         this.tail = null;
+      }
+
+      return temp;
+   }
 }
 
 const myLinkedList = new LinkedList(1);
-myLinkedList.push(10)
-console.log(myLinkedList)
+myLinkedList.push(10);
+myLinkedList.unshift(0);
+myLinkedList.shift();
+// myLinkedList.pop();
+console.log(myLinkedList);
